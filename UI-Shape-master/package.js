4b5c588abfe614299be2953849247b542f8087e0
Jimmy <jim.me.ryan@gmail.com>
2016-08-24 14:41:29 +0100

    uploading to github

---
+++ UI-Shape-master/package.js
@@ -0,0 +1,15 @@
+
+Package.describe({
+  name    : 'semantic:ui-shape',
+  summary : 'Semantic UI - Shape: Single component release',
+  version : '2.2.2',
+  git     : 'git://github.com/Semantic-Org/UI-Shape.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    'shape.css',
+    'shape.js'
+  ], 'client');
+});
---
+++ app.js
@@ -0,0 +1,16 @@
+
+$('.left').on("click", function(event) {
+    $('.shape').shape('flip left')
+});
+
+$('.right').on("click", function(event) {
+    $('.shape').shape('flip right')
+});
+
+$('.top').on("click", function(event) {
+    $('.shape').shape('flip up')
+});
+
+$('.bottom').on("click", function(event) {
+    $('.shape').shape('flip down')
+});
