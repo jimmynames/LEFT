6adef3f07267c5ecacf33b3eefac9bc714d9bb3b
Jimmy <jim.me.ryan@gmail.com>
2016-08-25 12:29:39 +0100

    added sounccloud API and widget

---
+++ sc.js
@@ -0,0 +1,14 @@
+
+
+SC.initialize({
+  client_id: 'cb119803d4123d468c762b4d81f70513'
+});
+
+var track_url = 'https://soundcloud.com/l_e_f_t/trust';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+});
+
+SC.oEmbed('https://soundcloud.com/l_e_f_t/trust', {
+    element: document.getElementById('widget')
+  });
