21c39eb864a47a0d83e8e9c98e154052d5a21aee
Jimmy <jim.me.ryan@gmail.com>
2016-08-27 20:49:01 +0100

    adding the start of mobile and iPad view

--- sc.js
+++ sc.js
@@ -4,16 +4,17 @@ SC.initialize({
   client_id: 'cb119803d4123d468c762b4d81f70513'
 });
 
-var track_url= 'https://soundcloud.com/l_e_f_t/trust';
+/*var track_url= 'https://soundcloud.com/l_e_f_t/trust';
 SC.oEmbed(track_url, 
           { auto_play: true },
           {theme_color: "#421C52"} 
          )
     .then(function(oEmbed) {
         console.log('oEmbed response: ', oEmbed);
-});
+}); */
 
-SC.oEmbed('https://soundcloud.com/l_e_f_t/trust', {
+SC.oEmbed('https://soundcloud.com/l_e_f_t/trust&amp;color=421C52&amp;auto_play=true', {
+    
     element: document.getElementById('widget')
   });
 
